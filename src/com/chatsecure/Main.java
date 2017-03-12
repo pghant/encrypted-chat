c5b3a6b2d703df6548555b40236978167a30c24b
Prasant Ghantasala <prasant.ghantasala@gmail.com>
2017-03-11 19:17:45 -0500

    Add SHA512 utility class and test method

--- src/com/chatsecure/Main.java
+++ src/com/chatsecure/Main.java
@@ -2,7 +2,18 @@ package com.chatsecure;
 
 public class Main {
 
+    public static void testSHA(String toHash) {
+        byte[] bytes = SHA512.hash(toHash.getBytes());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i< bytes.length; i++){
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        System.out.println(sb.toString());
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        testSHA("a random password");
+        testSHA("a random password");
+        testSHA("a random password");
     }
 }
