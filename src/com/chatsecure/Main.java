ea0c69958d35f64fca6d87e867c86230087a1528
Prasant Ghantasala <prasant.ghantasala@gmail.com>
2017-03-12 22:24:26 -0400

    Start sha256 implementation

--- src/com/chatsecure/Main.java
+++ src/com/chatsecure/Main.java
@@ -11,9 +11,17 @@ public class Main {
         System.out.println(sb.toString());
     }
 
+    private static void findK(int L) {
+        // K is the minimum number >= 0 such that L + 1 + K + 64 is a multiple of 512
+
+        // message needs to be multiple of 512 bits long (i.e. multiple of 64 bytes long)
+        // first append new byte 0x80 to message
+        // next append enough 0 bytes such that length + 8 is divisible by 64
+        // next append L as 8 bytes
+    }
+
     public static void main(String[] args) {
-        testSHA("a random password");
-        testSHA("a random password");
-        testSHA("a random password");
+        int L = 174; // bytes
+        System.out.println((int)Math.ceil((L + 9.0) / 64) * 64 - (L + 9));
     }
 }
